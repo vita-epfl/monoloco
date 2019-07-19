65e197f50da9c208178d806934ff6456387e0255
lorenzo <lorenzo.bertoni@yahoo.it>
2019-07-19 16:49:31 +0200

    add eggs file in gitignore

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+# extract version from __init__.py
+with open('monoloco/__init__.py', 'r') as f:
+    VERSION_LINE = [l for l in f if l.startswith('__version__')][0]
+    VERSION = VERSION_LINE.split('=')[1].strip()[1:-1]
+
+setup(
+    name='monoloco',
+    version=VERSION,
+    packages=[
+        'monoloco',
+        'monoloco.train',
+        'monoloco.predict',
+        'monoloco.eval',
+        'monoloco.prep',
+        'monoloco.visuals',
+        'monoloco.utils'
+    ],
+    license='GNU AGPLv3',
+    description='MonoLoco: Monocular 3D Pedestrian Localization and Uncertainty Estimation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Lorenzo Bertoni',
+    author_email='lorenzo.bertoni@epfl.ch',
+    url='https://github.com/vita-epfl/monoloco',
+    zip_safe=False,
+
+    install_requires=[
+        'openpifpaf',
+        'nuscenes-devkit',  # for nuScenes dataset preprocessing
+        'tabulate',   # For evaluation
+        'pylint',
+        'pytest',
+    ],
+)
