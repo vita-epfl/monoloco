5c695d3cea337872098771e1af9b561f0953310d
lorenzo <lorenzo.bertoni@yahoo.it>
2019-07-26 11:22:46 +0200

    fix import in main directory

--- monoloco/__init__.py
+++ monoloco/__init__.py
@@ -2,4 +2,3 @@
 """Open implementation of MonoLoco."""
 
 __version__ = '0.4.3'
-
--- monoloco/run.py
+++ monoloco/run.py
@@ -100,8 +100,8 @@ def main():
             from .visuals.webcam import webcam
             webcam(args)
         else:
-            from predict import predict
-            predict(args)
+            from . import predict
+            predict.predict(args)
 
     elif args.command == 'prep':
         if 'nuscenes' in args.dataset:
