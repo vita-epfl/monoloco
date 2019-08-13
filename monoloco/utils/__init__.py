ff5db1d872b843f6cfdaa25ae7830a4e3bfa10fa
lorenzo <lorenzo.bertoni@yahoo.it>
2019-08-13 10:20:54 +0200

    cleaning

--- monoloco/utils/__init__.py
+++ monoloco/utils/__init__.py
@@ -4,6 +4,5 @@ from .misc import get_task_error, get_pixel_error, append_cluster, open_annotati
 from .kitti import check_conditions, get_category, split_training, parse_ground_truth, get_calibration, factory_basename
 from .camera import xyz_from_distance, get_keypoints, pixel_to_camera, project_3d
 from .logs import set_logger
-from .old_stereo import depth_from_disparity_old
 from .stereo import depth_from_disparity
 from ..utils.nuscenes import select_categories
