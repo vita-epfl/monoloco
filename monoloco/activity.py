c5709a851390a8dab5e6d413f183e7e12f0f55b4
charlesbvll <charles.beauville@epfl.ch>
2021-05-14 15:57:26 +0200

    Minor change

--- monoloco/activity.py
+++ monoloco/activity.py
@@ -115,8 +115,6 @@ def is_raising_hand(kp):
     if is_right_risen:
         return 'right'
 
-    return None
-
 
 def check_f_formations(idx, idx_t, centers, angles, radii, social_distance=False):
     """
